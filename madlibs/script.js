d41ab056ea0a57c5cb6cab33598575dc380f7cdb
Khalid@monex.io <khalid@monex.io>
2015-10-17 00:04:32 -0700

    Take 2

--- madlibs/script.js
+++ madlibs/script.js
@@ -1,15 +1,17 @@
 // Code goes here
-
 angular.module('myApp', [])
-.controller('oneController', ['$scope', function($scope) {
-  $scope.female_name = "Female Name";
-  $scope.job_title = "Job Titile";
+.controller('oneController', function($scope) 
+{
+  $scope.female_name = {
+    text: 'Female Name'
+  };
+  
+  $scope.job_title = 'Job Titile';
   $scope.tedious_task = "Tedious Task";
   $scope.dirty_task = "Dirty Task";
   $scope.celebrity = "Celebrity";
   $scope.useless_skill = "Useful Skill";
   $scope.adjective = "Adjective";
   $scope.obnoxiuous_celbertity = "obnoxiuous_celbertity";
-  $scope.huge_number = "huge_number" 
-    
-}]);
+  $scope.huge_number = "huge_number";
+});
