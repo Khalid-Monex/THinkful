def4601e712f61892711af5983ed62f70931d213
Khalid@monex.io <khalid@monex.io>
2015-10-20 17:20:27 -0700

    Adding Biolerplate

--- madlibs/script.js
+++ madlibs/script.js
@@ -1,12 +1,13 @@
 // Code goes here
+
 angular.module('myApp', [])
-.controller('oneController', function($scope) 
-{
+.controller('oneController', function($scope) {
   $scope.female_name = { text: 'fn' };
   $scope.job_title = { text: 'jt' };
-  
+    
   $scope.set = function(name, title) {
             $scope.female_name.text = name;
             $scope.job_title.text = title;
         }
+
 });
